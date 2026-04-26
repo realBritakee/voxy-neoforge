231a89a97515005c72ee67b2ea2d75e8f7b85a33
Britakee <127439938+realBritakee@users.noreply.github.com>
2026-04-26 19:22:54 +0200

    Test

--- src/main/java/me/cortex/voxy/client/ICheekyClientChunkCache.java
+++
@@ -1,9 +0,0 @@
-package me.cortex.voxy.client;
-
-import net.minecraft.world.level.chunk.LevelChunk;
-import org.jetbrains.annotations.Nullable;
-
-public interface ICheekyClientChunkCache {
-    @Nullable
-    LevelChunk voxy$cheekyGetChunk(int x, int z);
-}
