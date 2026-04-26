2cbedaec8af829e277f4682099e19e05e21eaa33
Britakee <127439938+realBritakee@users.noreply.github.com>
2026-04-26 19:25:35 +0200

    Test

--- src/main/java/me/cortex/voxy/client/core/util/StairBlockCooked.java
+++
@@ -1,9 +0,0 @@
-package me.cortex.voxy.client.core.util;
-
-import net.minecraft.world.level.block.state.BlockState;
-
-public interface StairBlockCooked {
-
-    BlockState setBaseWaterlogged(BlockState blockState);
-
-}
